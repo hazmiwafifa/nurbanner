bc8c1bb9e4b325b6d896318dcd3f04abd37efb3e
hazmiwafifa <muhammadhazmiwafifa@gmail.com>
2025-09-25 07:14:31 +0700

    first commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,30 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+let slider = document.querySelector("#slider");
+let list_items = document.querySelectorAll("#nav-menu ul li");
+
+let index_value = 0;
+let left_position = 0;
+
+list_items.forEach((list_item, index) => {
+  //setting intial left position and width of slider.
+  slider.style.width = list_items[0].clientWidth + "px";
+  slider.style.width = "0px";
+
+  list_item.onclick = function () {
+    slider.style.width = list_item.clientWidth + "px";
+    index_value = index;
+    get_left_position();
+    slider.style.left = left_position + "px";
+    left_position = 0;
+  };
+});
+
+function get_left_position() {
+  for (let i = 0; i < index_value; i++) {
+    const element = list_items[i];
+    left_position += element.clientWidth;
+  }
+}
